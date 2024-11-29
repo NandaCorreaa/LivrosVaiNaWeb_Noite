1eabd48aca3fd628de08c2dbb93b3471f0277f65
seu nome <teste@exemplo.com>
2024-11-29 19:59:37 -0300

    Adicionando: Finalização Início, Footer, Quero Doar e Livros Doados.

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,20 @@
+import livroProtagonista from '../../assets/livroProtagonista.png'
+import s from './livrosDoados.module.scss'
+
 export default function LivrosDoados(){
     return(
-        <h2>EU SOU O COMPONETE LIVROS DOADOS</h2>
+        <section className={s.livrosDoadosSection}>
+            <h2>Livros Doados</h2>
+            <section className={s.containerCards}>
+                <section>
+                    <img src={livroProtagonista} alt="Imagem do livro O protagonista" />
+                    <div>
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
     )
 }
